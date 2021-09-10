33c0f0a05ed5e114f7f3f9b29532c744d0a789ac
Sascha Collinet <collinet.sascha@gmail.com>
2021-09-10 13:02:19 +0200

    final setup

--- main.js
+++ main.js
@@ -8,11 +8,7 @@ fetch(url)
     });
  
 function displayData(data) {
-    // grab the div element from the html file
     let charInfo = document.getElementById("display-info");
-    //loop through the json data, and create a paragraph element
-    //for each name, and append it to the div element in the html
-    //file
     data.forEach((character) => {
         let charName = document.createElement("p");
         charName.innerHTML = character.name;
